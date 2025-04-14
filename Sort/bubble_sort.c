0b25eaabca939a895e01110507c6c3d04a881bb3
david <david287391456@gmail.com>
2025-04-14 10:47:10 +0800

    add_improvement

--- Sort/bubble_sort.c
+++ Sort/bubble_sort.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define len 5
 void bubble_sort(int *);
-
+void bubble_sort_improve(int *);
 int main()
 {
     int arr[len]={6,5,2,1,3};
-
-    bubble_sort(arr);
+    int arr1[len]={1,2,3,4,5};
+    bubble_sort_improve(arr1);
     int i;
     for(i=0;i<len;i++)
     {
-        printf("%d",arr[i]);
+        printf("%d",arr1[i]);
     }
 }
 
@@ -30,4 +31,30 @@ void bubble_sort(int *arr)
             }
         }
     }
+}
+//add flag:best O(n)
+void bubble_sort_improve(int *arr)
+{
+    int i,j;
+    bool swap=false;
+    for(i=0;i<len;i++)
+    {
+        for(j=0;j<len-1-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap=true;
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+        if(!swap)
+        {
+            printf("Best case\n");
+            break;
+        }
+            
+    }
+
 }
